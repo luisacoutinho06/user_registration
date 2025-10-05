b96c020aa95220fb1dc4e13b4dff8fbcac6e77a3
Luisa <104804096+luisacoutinho06@users.noreply.github.com>
2025-10-05 14:27:35 -0300

    Removendo connectionstring referente ao sqllite e implementando a conexão referente o sql server.

--- src/backend/Domain/Entities/EntityBase.cs
+++ src/backend/Domain/Entities/EntityBase.cs
@@ -3,6 +3,6 @@
     public abstract class EntityBase
     {
         public int Id { get; set; }
-        public DateTime RegistrationData { get; set; } = DateTime.UtcNow;
+        public DateTime RegistrationDate{ get; set; } = DateTime.UtcNow;
     }
 }
---
+++ src/backend/Infrastructure/Migrations/20251005172639_Migration0001.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migration0001 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "RegistrationData",
+                table: "Users",
+                newName: "RegistrationDate");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "RegistrationDate",
+                table: "Users",
+                newName: "RegistrationData");
+        }
+    }
+}
